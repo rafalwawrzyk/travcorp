e9ee0dedaf90f4912bf39946d0a5fc3e87222ac3
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2022-07-11 16:24:12 +0200

    [feature/doc] add helpers docs

--- trav/src/helpers/index.js
+++ trav/src/helpers/index.js
@@ -1,4 +1,9 @@
-// round score helper for stars
+/**
+ * Round score based on data
+ * @param {number} score 
+ * @returns rounded score if decimals greater than 0.5 or flored if less
+ */
+
 export const roundScore = (score) => {
   const flored = Math.floor(score);
   const rounded = Math.round(score);
@@ -11,14 +16,26 @@ export const roundScore = (score) => {
   return decimal > 0.5 ? rounded : flored + 0.5;
 };
 
-// format currency and remvoe last digits
+/**
+ * Price formatter based on data, with possibility to change currency
+ * @param {number} number 
+ * @param {string} curr 
+ * @returns formated price depended on locale and remove last digits to fit design
+ */
+
 export const currFormatter = (number, curr = "EUR") => {
   return new Intl.NumberFormat("en-us", { style: "currency", currency: curr })
     .format(number)
     .replace(/\.00$/, "");
 };
 
-// create plurals depended on data
+/**
+ * Plural format of data depended on data
+ * @param {number} data
+ * @param {string} plural
+ * @param {string} single    
+ * @returns string with plural or singular format
+ */
 export const pluralFormat = (data, plural, single) => {
   return data !== 1 ? `${data} ${plural}` : `${data} ${single}`;
 }
