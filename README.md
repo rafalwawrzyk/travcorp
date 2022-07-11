d1471a1805c32b5b4ead24670f22361225c955a8
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2022-07-11 12:37:04 +0200

    [feature/readme] add README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+## Tech Stack
+
+**Client:** React
+
+**Server:** Node, Express
+
+
+## Installation
+
+Install project with npm follow these steps:
+
+#### Clone the project:
+```bash
+  git clone https://github.com/rafalwawrzyk/travcorp.git
+```
+
+#### Backend:
+```bash
+  cd server 
+  
+  npm install
+
+  npm start
+```
+
+#### Frontend:
+```bash
+  cd trav 
+  
+  npm install
+
+  npm start
+```
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 // imports
 const express = require('express');
 const trips_routes = require('./routes/trips.js');
+const cors = require('cors');
 
 // set up ports
 const PORT = process.env.PORT || 5000;
@@ -8,10 +9,13 @@ const PORT = process.env.PORT || 5000;
 // run express
 const app = express();
 
+// set up cors
+app.use(cors());
+
 // start server
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
-})
+});
 
 // json middleware
 app.use(express.json());
